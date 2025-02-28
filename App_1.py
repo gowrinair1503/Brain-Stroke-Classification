63f4e6e6d51626451b4262469514fe94802f64c2
gowrinair1503 <cse.210840131002@gmail.com>
2025-02-28 10:26:04 +0530

    Update App_1.py

--- App_1.py
+++ App_1.py
@@ -65,7 +65,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     # Preprocess & predict
     processed_image = preprocess_image(image)
