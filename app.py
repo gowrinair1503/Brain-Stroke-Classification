75b757ef7c67519a73f85b9d4ea90c380b40a340
Deep Kothari <deepkothari55@gmail.com>
2025-04-21 12:12:09 +0530

    add featuer adn rename App_1.py to app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-import streamlit as st
-import tensorflow as tf
+import streamlit as st 
+
+import tensorflow as tf  
+# from tensorflow import keras
 import numpy as np
 from PIL import Image
 
 # Load the trained model
 @st.cache_resource
 def load_model():
-    model = tf.keras.models.load_model('model.h5')  # Ensure 'model.h5' is in your repo
+    model = tf.keras.models.load_model('model.h5')  # type: ignore # Ensure 'model.h5' is in your repo
     return model
 
 model = load_model()
@@ -14,6 +16,7 @@ model = load_model()
 # Function to preprocess the image
 def preprocess_image(image):
     image = image.resize((100, 100))  # Resize to match training input
+    image = image.convert('RGB')
     image = np.array(image) / 255.0   # Normalize
     image = np.expand_dims(image, axis=0)  # Add batch dimension
     return image
@@ -70,8 +73,9 @@ if uploaded_file is not None:
     # Preprocess & predict
     processed_image = preprocess_image(image)
     prediction = model.predict(processed_image)[0][0]
+    print(type(prediction))
 
     # Display result
-    result = "🔴 Stroke Detected" if prediction >= 0.5 else "🟢 No Stroke Detected"
-    
+    result = f"🔴 Stroke Detected with {round((float(prediction) * 100), 2)}% Confidence"   if prediction >= 0.5 else "🟢 No Stroke Detected"
+    roundd = round((prediction * 100), 2)
     st.markdown(f'<div class="result-box">{result}</div>', unsafe_allow_html=True)
